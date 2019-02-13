881fcc78ce9e820c144c39d0770cff51325e8f4f
mjan-cea <Mathieu.Jan@cea.fr>
2019-02-13 11:53:41 -0800

    Ok we can retrieve data from memory and getting instructions

--- test/test01.s
+++ test/test01.s
@@ -1,8 +1,10 @@
 #
 # Expected Result: r1 = 5 & r2 = 10
 #
-        .word   28;
-main:	
+
+        .globl _start;
+# To set the ELF entry point	
+_start:                                                                 
         add    $r1  = $r0 , 5;
         add    $r2  = $r1 , 5;
 	
