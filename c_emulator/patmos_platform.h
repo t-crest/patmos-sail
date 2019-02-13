3cc7ce1df464d2469aed62d64a4c5e2f455395bf
mjan-cea <Mathieu.Jan@cea.fr>
2019-02-13 11:53:30 -0800

    Ok we can retrieve data from memory and getting instructions

--- c_emulator/patmos_platform.h
+++ c_emulator/patmos_platform.h
@@ -7,6 +7,3 @@ mach_32bits plat_ram_base(unit);
 mach_32bits plat_ram_size(unit);
 bool within_phys_mem(mach_32bits, sail_int);
 
-mach_32bits plat_rom_base(unit);
-mach_32bits plat_rom_size(unit);
-
--- c_emulator/patmos_platform_impl.h
+++ c_emulator/patmos_platform_impl.h
@@ -5,12 +5,8 @@
 
 /* Settings of the platform implementation. */
 
-#define DEFAULT_RSTVEC     0x00001000
 #define SAIL_XLEN          32
 
 extern uint32_t rv_ram_base;
 extern uint32_t rv_ram_size;
-
-extern uint32_t rv_rom_base;
-extern uint32_t rv_rom_size;
 
