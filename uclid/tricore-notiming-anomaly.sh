c95fa5218754b4dd689aac5af862a47979f7be04
BINDER Benjamin <benjamin.binder@cea.fr>
2020-03-24 21:43:11 +0100

    Setting actual depths (tricore/no-stalling & stalling-before): same for all.

--- uclid/tricore-notiming-anomaly.sh
+++ uclid/tricore-notiming-anomaly.sh
@@ -3,17 +3,20 @@
 case $1 in
 	"no-stalling")
 		echo "no-stalling-onmiss"
-		time uclid src/tricore/common.ucl src/tricore/tricore-nostalling-onmiss.ucl test/tricore/test.ucl test/tricore/param-notiming-anomaly-nostalling.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly.ucl -no-version-check | less
+        sed -n 2,3p test/tricore/param-notiming-anomaly-nostalling.ucl
+		time uclid src/tricore/common.ucl src/tricore/tricore-nostalling-onmiss.ucl test/tricore/test.ucl test/tricore/param-notiming-anomaly-nostalling.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly.ucl -no-version-check
 		;;
 		
 	"stalling-before")
 		echo "stalling-before-onmiss"
-		time uclid src/tricore/common.ucl src/tricore/tricore-stalling-before-onmiss.ucl test/tricore/test.ucl test/tricore/param-notiming-anomaly-stalling-before.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly-stalling-before.ucl -no-version-check | less
+        sed -n 2,3p test/tricore/param-notiming-anomaly-stalling-before.ucl
+		time uclid src/tricore/common.ucl src/tricore/tricore-stalling-before-onmiss.ucl test/tricore/test.ucl test/tricore/param-notiming-anomaly-stalling-before.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly-stalling-before.ucl -no-version-check
 		;;
 		
  	"stalling-whole")
 		echo "stalling-whole-onmiss"
-		time uclid src/tricore/common.ucl src/tricore/tricore-stalling-whole-onmiss.ucl test/tricore/test.ucl test/tricore/param-notiming-anomaly-stalling-whole.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly-stalling-whole.ucl -no-version-check | less
+        sed -n 2,3p test/tricore/param-notiming-anomaly-stalling-whole.ucl
+		time uclid src/tricore/common.ucl src/tricore/tricore-stalling-whole-onmiss.ucl test/tricore/test.ucl test/tricore/param-notiming-anomaly-stalling-whole.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly-stalling-whole.ucl -no-version-check
 		;;
 
 	*)
