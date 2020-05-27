1ec4843b020e42af1ae9877d8908fa1ddea2d49d
BINDER Benjamin <benjamin.binder@cea.fr>
2020-05-27 12:34:49 +0200

    New set of eval & partial results

--- uclid/tricore-notiming-anomaly.sh
+++ uclid/tricore-notiming-anomaly.sh
@@ -3,28 +3,12 @@
 a="time uclid src/tricore/common.ucl"
 
 case $1 in
-	"basic")
-        sed -n 6p test/tricore/param-basic.ucl
-        sed -n 9,11p test/tricore/param-basic.ucl
-        a="$a src/tricore/tricore.ucl test/tricore/param-basic.ucl"
-    ;;
-
-    "reduc")
-        sed -n 6p test/tricore/param-reduc.ucl
-        sed -n 9,11p test/tricore/param-reduc.ucl
-        a="$a src/tricore/tricore.ucl test/tricore/param-reduc.ucl"
-    ;;
-    
-    "reduc-hazards")
-        sed -n 6p test/tricore/param-reduc-hazards.ucl
-        sed -n 9,11p test/tricore/param-reduc-hazards.ucl
-        a="$a src/tricore/tricore.ucl test/tricore/param-reduc-hazards.ucl"
-    ;;
-    
-    "reduc-hazards-SB")
-        sed -n 6p test/tricore/param-reduc-hazards-SB.ucl
-        sed -n 9,11p test/tricore/param-reduc-hazards-SB.ucl
-        a="$a src/tricore/tricore-SB.ucl test/tricore/param-reduc-hazards-SB.ucl"
+	"basic" | "pipeline_reduc" | "general_reduc" | "hazards_noSB" | "hazards_noSB_reduc" | "hazards_all_reduc")
+        sed -n 8p test/tricore/param-$1.ucl
+        sed -n 11,15p test/tricore/param-$1.ucl
+        a="$a src/tricore/tricore-SB.ucl test/tricore/param-$1.ucl test/tricore/test.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly.ucl -no-version-check"
+        echo $a
+        eval $a
     ;;
     
     *)
@@ -32,7 +16,3 @@ case $1 in
         exit
     ;;
 esac
-
-a="$a test/tricore/test.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly.ucl -no-version-check"
-echo $a
-eval $a
