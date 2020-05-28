960f1a4309e8090ab2824036d61d4df59d20a2bb
BINDER Benjamin <benjamin.binder@cea.fr>
2020-05-28 10:40:47 +0200

    New eval config

--- uclid/tricore-notiming-anomaly.sh
+++ uclid/tricore-notiming-anomaly.sh
@@ -3,7 +3,7 @@
 a="time uclid src/tricore/common.ucl"
 
 case $1 in
-	"basic" | "pipeline_reduc" | "general_reduc" | "hazards_noSB" | "hazards_noSB_reduc" | "hazards_all_reduc")
+	"basic" | "pipeline_reduc" | "general_reduc" | "hazards" | "hazards_noSB" | "hazards_noSB_reduc" | "hazards_all_reduc")
         sed -n 8p test/tricore/param-$1.ucl
         sed -n 11,15p test/tricore/param-$1.ucl
         a="$a src/tricore/tricore-SB.ucl test/tricore/param-$1.ucl test/tricore/test.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly.ucl -no-version-check"
