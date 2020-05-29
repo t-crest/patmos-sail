64424ca68b450497abc99e701702fb7da5d02ed9
BINDER Benjamin <benjamin.binder@cea.fr>
2020-05-29 21:57:17 +0200

    Final set of evaluations & results

--- uclid/tricore-notiming-anomaly.sh
+++ uclid/tricore-notiming-anomaly.sh
@@ -3,9 +3,9 @@
 a="time uclid src/tricore/common.ucl"
 
 case $1 in
-	"basic" | "pipeline_reduc" | "general_reduc" | "hazards" | "hazards_noSB" | "hazards_noSB_reduc" | "hazards_all_reduc")
-        sed -n 8p test/tricore/param-$1.ucl
-        sed -n 11,15p test/tricore/param-$1.ucl
+    "pipeline_reduc" | "pipeline_general_reduc" | "reduc_memdep" | "dual_pipeline_memdep" | "waw" | "waw_reduc" | "waw_reduc_noSB" | "waw_noSB")
+        sed -n 9p test/tricore/param-$1.ucl
+        sed -n 12,17p test/tricore/param-$1.ucl
         a="$a src/tricore/tricore-SB.ucl test/tricore/param-$1.ucl test/tricore/test.ucl test/tricore/conditions-notiming-anomaly.ucl test/tricore/property-notiming-anomaly.ucl -no-version-check"
         echo $a
         eval $a
